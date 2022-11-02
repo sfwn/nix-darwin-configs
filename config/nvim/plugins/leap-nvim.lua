4db3cbab15020aaa743a395b71638a749c3ebd17
sfwn <sfwnlin@gmail.com>
2022-11-02 20:28:33 +0800

    nvim: leap search everywhere now

--- config/nvim/plugins/leap-nvim.lua
+++ config/nvim/plugins/leap-nvim.lua
@@ -1,17 +1,23 @@
-require('leap').add_default_mappings()
-
--- Disable auto-jumping to the first match
-require('leap').opts.safe_labels = {}
-
--- bidirectional search
--- require('leap').leap { target_windows = { vim.fn.win_getid() } }
-
--- search in all windows
--- require('leap').leap { target_windows = vim.tbl_filter(
---   function (win) return vim.api.nvim_win_get_config(win).focusable end,
---   vim.api.nvim_tabpage_list_wins(0)
--- )}
+-- require('leap').add_default_mappings()
 
 -- How to live without `s`/`S`/`x`/`X`?
 -- s = cl
 -- S = cL
+
+local leap = require("leap")
+
+local function search_all()
+    leap.leap({
+        opts = {
+            -- Disable auto-jumping to the first match
+            safe_labels = {},
+        },
+        target_windows = vim.tbl_filter(
+            function(win) return vim.api.nvim_win_get_config(win).focusable end,
+            vim.api.nvim_tabpage_list_wins(0)
+        ),
+    })
+end
+
+-- custom mapping
+vim.keymap.set({ 'n', 'x', 'o' }, '<C-s>', search_all, { noremap = true })
