bba46a2e3f38036874bd7e8059fa5de46643ff13
sfwn <sfwnlin@gmail.com>
2022-11-14 00:37:30 +0800

    nvim: null-ls goimports auto get local package

--- config/nvim/plugins/null-ls-nvim.lua
+++ config/nvim/plugins/null-ls-nvim.lua
@@ -4,6 +4,15 @@ local formatting = null_ls.builtins.formatting
 null_ls.setup({
     debug = true,
     sources = {
-        formatting.goimports,
+        formatting.goimports.with(
+            {
+                -- https://github.com/jose-elias-alvarez/null-ls.nvim/discussions/994#discussioncomment-3351235
+                extra_args = function(params)
+                    local cmd = string.format("head -1 %s/go.mod | awk '{print $2}'", params.root)
+                    local output = vim.split(vim.fn.system(cmd), "\n")[1]
+                    return { "-v", "-local", output }
+                end,
+            }
+        ),
     },
 })
